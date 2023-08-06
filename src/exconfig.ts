1b0a77019267ebb68d864523e309079868f2bc05
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-08-06 18:52:45 -0300

    Update config to match localhost

--- src/exconfig.ts
+++ src/exconfig.ts
@@ -4,21 +4,21 @@ import { EVMNetwork } from './pages/Background/types/network';
 export default {
   enablePasswordEncryption: false,
   showTransactionConfirmationScreen: true,
-  factory_address: '0x9406Cc6185a346906296840746125a0E44976454',
+  factory_address: '0x8c35464a19Dd798F6f3986b9b2a31FC4E558C181',
   stateVersion: '0.1',
   network: {
-    chainID: '11155111',
+    chainID: '31337',
     family: 'EVM',
-    name: 'Sepolia',
-    provider: 'https://sepolia.infura.io/v3/bdabe9d2f9244005af0f566398e648da',
+    name: 'Localhost',
+    provider: 'http://127.0.0.1:8545/',
     entryPointAddress: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
-    bundler: 'https://sepolia.voltaire.candidewallet.com/rpc',
+    bundler: 'http://127.0.0.1:3000/rpc',
     baseAsset: {
       symbol: 'ETH',
       name: 'ETH',
       decimals: 18,
       image:
-        'https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/6ed5f/eth-diamond-black.webp',
+        'https://ethereum.org/static/2aba39d4e25d90caabb0c85a58c6aba9/f41b0/eth-glyph-colored.png',
     },
   } satisfies EVMNetwork,
 };
